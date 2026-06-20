03d13cbcaad1da782ee864c9bdd3c51ce3a7b47d
Rey Gato <arizagaalex97@gmail.com>
2026-06-20 17:15:28 -0400

    Primera version

---
+++ app.py
@@ -0,0 +1,382 @@
+
+import streamlit as st
+from funcion import*
+from streamlit_option_menu import option_menu
+import os
+set_bg_local("imagenes/fondo-de-pantalla-negro.png")
+
+if "selected_page" not in st.session_state:
+    st.session_state["selected_page"] = "menu"
+
+# Mostrar menú principal
+if st.session_state["selected_page"] == "menu":
+
+    st.markdown(
+        """
+        <h1 style='text-align:center;'>
+            📚 Calculadora de Cálculo
+        </h1>
+        <h4 style='text-align:center;'>
+            Seleccione una opción
+        </h4>
+        """,
+        unsafe_allow_html=True
+    )
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button(
+            "🧮 Calculadora",
+            use_container_width=True
+        ):
+            st.session_state["selected_page"] = "Calculadora"
+            st.rerun()
+
+    with col2:
+        if st.button(
+            "📈 Gráficas 3D",
+            use_container_width=True
+        ):
+            st.session_state["selected_page"] = "Graficas 3d"
+            st.rerun()
+        st.markdown("---")
+
+    col1, col2 = st.columns([1, 2])
+
+    with col1:
+        st.image("imagenes/images.png", width=250)
+
+    with col2:
+        st.markdown("""
+        ## Bienvenido
+
+        Esta aplicación permite resolver problemas de cálculo diferencial e integral.
+
+        ### Funciones disponibles
+        - Límites
+        - Regla de L'Hôpital
+        - Integración por partes
+        - Gráficas 2D
+        - Gráficas 3D
+        """)
+
+    st.markdown("---")
+
+    st.markdown("### Ejemplos")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.latex(r"\lim_{x\to0}\frac{\sin(x)}{x}")
+
+    with col2:
+        st.latex(r"\int x e^x\,dx")
+
+    with col3:
+        st.latex(r"f(x,y)=x^2+y^2")
+
+    st.stop()
+
+    st.stop()
+st.title("CALCULADORA")
+
+selected = st.session_state["selected_page"]
+st.session_state["selected_page"] = selected
+if selected == 'Calculadora': 
+  if "opcion_actual" not in st.session_state:
+        st.session_state["opcion_actual"] = "Limites"
+  opcion = option_menu(None, ["Limites", "L Hopital", "Integracion por partes"], 
+  icons=["bi bi-calculator", "bi bi-arrow-down-right","bi bi-card-checklist"], menu_icon="cast", default_index=0, orientation="horizontal") 
+  opcion 
+#Limites 
+  if opcion == "Limites": 
+    st.subheader("Calculadora de limites✌") 
+    func_input = st.text_input("Ingrese la Funcion f(x): ", "sin(x)/x")
+    valor=st.number_input("Tiende a: ",value=0.0) 
+    dir_lim=st.selectbox("Direccion: ",["ambos","+","-"])
+    if requiere_3d(func_input):
+        st.info(
+        "📈 La expresión contiene la variable 'y'. "
+        "Se recomienda utilizar el módulo de Gráficas 3D."
+         )
+        st.success("📈 abra el graficador 3d")
+        if st.button("🚀 Ir a Graficas 3D"):
+    
+          st.session_state["ultima_funcion"] = func_input
+          st.session_state["selected_page"] = 'Graficas 3d'
+
+          st.rerun()
+    if st.button("Calcular"): 
+      x = sp.symbols('x') 
+      try:
+       f = validar_funcion(func_input)
+      except ValueError as e:
+       st.error(f"❌ {e}")
+       st.stop()
+      val_num = float(valor) 
+      st.write("### Procedimiento paso a paso") 
+      st.latex(rf"\lim_{{x \to {val_num}}} {sp.latex(f)}")
+      st.write("Sustituyendo el valor en la función:") 
+      evaluacion = f.subs(x, val_num) 
+      st.latex(rf"f({val_num}) = {sp.latex(evaluacion)}")
+      resultado = sp.limit(f, x, val_num)
+      st.write("Resultado final:") 
+      st.latex(rf"\lim_{{x \to {val_num}}} {sp.latex(f)} = {sp.latex(resultado)}") 
+      pasos = [
+        f"Función: {func_input}",
+        f"x tiende a {valor}",
+        f"Sustitución: f({valor}) = {evaluacion}"
+        ]
+      generar_pdf("limite.pdf",
+         "Calculadora de Limites",
+       pasos,
+       str(resultado),
+       "grafica.png"
+)
+    if st.button("Graficar"):
+      st.subheader("Visualizacion")
+      figura = graficar_funcion(func_input)
+      figura.savefig("grafica.png",
+      bbox_inches="tight" )
+      st.pyplot(figura)
+      with open("limite.pdf", "rb") as archivo:
+
+       st.download_button(
+        "📄 Descargar PDF",
+        archivo,
+        file_name="limite.pdf",
+        mime="application/pdf"
+      )
+
+#Lhopital
+  if opcion == "L Hopital":
+    st.subheader("Regla de L Hopital")
+    num=st.text_input("Numerador f(x); ", "sin(x)")
+    st.session_state["ultima_funcion"] = num
+    den = st.text_input("Denominador g(x): ","x")
+    val = st.number_input("Tiende a: ",value=0.0)
+    if requiere_3d(num):
+        st.info(
+        "📈 Se detectó la variable 'y'. "
+        "Para visualizar correctamente la función use Gráficas 3D."
+        )
+        st.success("📈 abra el graficador 3d")
+         
+        if st.button("🚀 Abrir Graficadora 3D"):
+    
+          st.session_state["ultima_funcion"] = num
+          st.session_state["selected_page"] = 'Graficas 3d'
+
+          st.rerun()
+    
+    if st.button("Aplicar L Hopital"):
+      x = sp.symbols('x')
+      try:
+        f = validar_funcion(num)
+        g = validar_funcion(den)
+      except ValueError as e:
+        st.error(f"❌ {e}")
+        st.stop()
+      val_num = float(val)
+      st.write("Veririficacion de condiciones")
+        #Validacion :v
+      lim_num = sp.limit(f,x,val_num)
+      lim_den = sp.limit(g,x,val_num)
+        #procedimiento de la verificacion
+      st.latex(rf"\lim_{{x \to {val}}} f(x) = {sp.latex(lim_num)}")
+      st.latex(rf"\lim_{{x \to {val}}} g(x) = {sp.latex(lim_den)}")
+        #verificacion de ideterminaciones
+      
+      if es_indeterminado(lim_num):
+        st.success("Se ha confirmado una indeterminacion")
+        
+        
+        #Proceso
+        f_p,g_p,res=aplicar_lhopital(num,den,val_num)
+        st.write("Procedimiento paso a paso: ")
+        #Exprecion original
+        st.latex(rf"\lim_{{x \to {val}}} \frac{{{sp.latex(f)}}}{{{sp.latex(g)}}}")
+        st.write("Aplicamos la regla de L Hopital: ")
+        #Mostramos las derivadas
+        st.latex(rf"\frac{{f'(x)}}{{g'(x)}} = \frac{{{sp.latex(f_p)}}}{{{sp.latex(g_p)}}}")
+        #Mostramos el resultado final 
+        st.write("Resultado:")
+        st.latex(rf"\lim_{{x \to {val}}} \frac{{{sp.latex(f_p)}}}{{{sp.latex(g_p)}}} = {sp.latex(res)}")
+      
+      
+        pasos = [
+          f"Numerador: {num}",
+          f"Denominador: {den}",
+          f"Limite numerador: {lim_num}",
+          f"Limite denominador: {lim_den}",
+          f"Derivada numerador: {f_p}",
+          f"Derivada denominador: {g_p}",
+          f"Resultado final: {res}"
+          ]
+        generar_pdf(
+           "lhopital.pdf",
+           "Regla de L'Hopital",
+           pasos,
+           str(res)
+            )
+
+
+      else:
+           st.warning("El limite no resulta en 0/0. puede que la regla de l hopital no se aplique correctamente")   
+
+    if st.button("Graficar Funciones"):
+       try:
+          validar_funcion(num)
+          validar_funcion(den)
+          st.subheader("Visualización")
+          col1, col2 = st.columns(2)
+          with col1:
+            st.write("f(x)")
+            figura_f = graficar_funcion(num)
+            figura_f.savefig(
+               "grafica_lhopital_f.png",
+                bbox_inches="tight"
+                )
+            st.pyplot(figura_f)
+
+          with col2:
+            st.write("g(x)")
+            figura_g = graficar_funcion(den)
+            figura_g.savefig(
+              "grafica_lhopital_g.png",
+               bbox_inches="tight"
+                )
+            st.pyplot(figura_g)
+
+       except ValueError as e:
+          st.error(f"❌ {e}")  
+    if os.path.exists("lhopital.pdf"):
+    
+     with open("lhopital.pdf", "rb") as archivo:
+
+        st.download_button(
+            "📄 Descargar PDF",
+            archivo,
+            file_name="lhopital.pdf",
+            mime="application/pdf"
+        )   
+#integracioj por partes
+  if opcion == "Integracion por partes":
+    st.subheader("Integracion por Partes")   
+    u_input = st.text_input("Ingresa u: ", "x")
+    st.session_state["ultima_funcion"] = u_input
+    dv_input = st.text_input("Ingresa dv: ", "exp(x)")
+    if requiere_3d(u_input):
+        st.info(
+        "📈 Se detectó una función de dos variables. "
+        "Puede visualizarla mejor en el módulo de Gráficas 3D."
+        
+        )
+        st.success("📈 abra el graficador 3d")
+        
+        if st.button("🚀 Abrir Graficadora 3D"):
+
+          st.session_state["ultima_funcion"] = u_input
+          st.session_state["selected_page"] = 'Graficas 3d'
+
+          st.rerun()
+    
+    
+    if st.button("Calcular"):
+        try:
+         validar_funcion(u_input)
+         validar_funcion(dv_input)
+
+        except ValueError as e:
+         st.error(f"❌ {e}")
+         st.stop()
+        u,dv,du,v,res=integrar_por_partes(u_input, dv_input)
+        st.write("### Procedimiento")
+        #mostrar la formula 
+        st.write("Identificación de componentes:")
+        st.latex(rf"u = {sp.latex(u)}, \quad dv = {sp.latex(dv)} dx")
+        #mostrar la derivada
+        st.write("Derivando $u$ e integrando $dv$:")
+        st.latex(rf"du = {sp.latex(du)} dx, \quad v = {sp.latex(v)}")
+        #aplicar la formula
+        st.write("3. Aplicando la fórmula $\\int u \\, dv = uv - \\int v \\, du$:")
+        st.latex(rf"\int {sp.latex(u)} \cdot {sp.latex(dv)} dx = ({sp.latex(u)})({sp.latex(v)}) - \int ({sp.latex(v)})({sp.latex(du)}) dx")
+        #resultado final
+        st.write("### Resultado final:")
+        st.latex(rf"= {sp.latex(res)} + C") 
+        pasos = [
+         f"u = {u}",
+         f"dv = {dv}",
+         f"du = {du}",
+         f"v = {v}",
+         f"Resultado = {res}"
+         ]
+        generar_pdf(
+         "integracion.pdf",
+         "Integracion por Partes",
+        pasos,
+        str(res)
+         )     
+    if st.button("Graficar Componentes"):
+      try:
+        validar_funcion(u_input)
+        validar_funcion(dv_input)
+        st.subheader("Visualización")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("u(x)")
+            figura_u = graficar_funcion(u_input)
+            figura_u.savefig(
+                "grafica_u.png",
+                bbox_inches="tight"
+            )
+            st.pyplot(figura_u)
+        with col2:
+            st.write("dv(x)")
+            figura_dv = graficar_funcion(dv_input)
+            figura_dv.savefig(
+                "grafica_dv.png",
+                bbox_inches="tight"
+            )
+            st.pyplot(figura_dv)
+      except ValueError as e:
+
+        st.error(f"❌ {e}")
+    if os.path.exists("integracion.pdf"):
+      with open("integracion.pdf", "rb") as archivo:
+
+        st.download_button(
+            "📄 Descargar PDF",
+            archivo,
+            file_name="integracion.pdf",
+            mime="application/pdf"
+      )        
+  if st.button("⬅ Regresar al menú principal"):
+    st.session_state["selected_page"] = "menu"
+    st.rerun()
+          
+if selected=='Graficas 3d': 
+    
+    st.subheader("📈 Graficador 3D")
+
+    funcion_3d = st.text_input(
+        "Función f(x,y):",
+        value=st.session_state.get(
+            "ultima_funcion",
+            "x**2 + y**2"
+        )
+    )
+
+    if st.button("Graficar 3D"):
+
+        try:
+            figura = graficar_3d(funcion_3d)
+            st.pyplot(figura)
+
+        except Exception as e:
+            st.error(f"❌ {e}")
+    if st.button("⬅ Regresar al menú principal"):
+       st.session_state["selected_page"] = "menu"
+       st.rerun()
+      
---
+++ funcion.py
@@ -0,0 +1,201 @@
+import sympy as sp
+import base64
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+#para descargar documentos
+from reportlab.platypus import (
+    SimpleDocTemplate,
+    Paragraph,
+    Spacer,
+    Image
+)
+from reportlab.lib.styles import getSampleStyleSheet
+#esto es de limites
+
+def calcular_limite(expresion, valor, direccion='+'):
+    x = sp.symbols('x')
+    try:
+        f = sp.sympify(expresion)
+        if not f.free_symbols.issubset({x}):            
+         if direccion == "ambos":
+            return sp.limit(f, x, valor)
+        return sp.limit(f, x, valor, dir=direccion)
+    except:
+        return None
+def calcular_derivada(exprecion):
+    x=sp.symbols('x')
+    f=sp.sympify(exprecion)
+    return sp.diff(f,x)
+def es_indeterminado(limite):
+    return (limite.is_zero) or (abs(limite).is_infinite)
+ #l,Hopital
+def aplicar_lhopital(num_str,den_str,valor):
+    x = sp.symbols('x')
+    f = sp.sympify(num_str)
+    g = sp.sympify(den_str)
+    # Calcular derivadas
+    f_p = sp.diff(f, x)
+    g_p = sp.diff(g, x)
+    # Calcular el límite 
+    resultado = sp.limit(f_p / g_p, x, valor)
+    # Retornamos todo lo necesario para el paso a paso
+    return f_p, g_p, resultado
+#Integral por partes
+def integrar_por_partes(u_str, dv_str):
+    x = sp.symbols('x')
+    u = sp.sympify(u_str)
+    dv = sp.sympify(dv_str)
+    
+    du = sp.diff(u,x)
+    v = sp.integrate(dv, x)
+    #resultado: uv - integrate(v*du)
+    
+    integral_parte = sp.integrate(v*du,x)
+    resultado= (u*v) - integral_parte
+    return u,dv,du,v,resultado
+def set_bg_local(file_path):
+    with open(file_path, "rb") as f:
+        img_bytes = f.read()
+    encoded = base64.b64encode(img_bytes).decode()
+    
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/png;base64,{encoded});
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+def graficar_funcion(expresion, rango=(-10, 10)):
+    x = sp.symbols('x')
+    f = sp.sympify(expresion)
+    f_num = sp.lambdify(x, f, 'numpy')
+
+    x_vals = np.linspace(rango[0], rango[1], 400)
+    y_vals = f_num(x_vals)
+    #parte garfica
+    fig, ax = plt.subplots()
+    ax.plot(x_vals, y_vals)
+    ax.set_title(f"Gráfica de f(x) = {expresion}")
+    ax.grid(True)
+    ax.axhline(0, color='black', linewidth=0.5)
+    ax.axvline(0, color='black', linewidth=0.5)
+    
+    return fig    
+def validar_funcion(expr_texto):
+    x, y = sp.symbols('x y')
+
+    permitidos = {
+        'x': x,
+        'y': y,
+        'sin': sp.sin,
+        'cos': sp.cos,
+        'tan': sp.tan,
+        'asin': sp.asin,
+        'acos': sp.acos,
+        'atan': sp.atan,
+        'exp': sp.exp,
+        'log': sp.log,
+        'sqrt': sp.sqrt,
+        'pi': sp.pi,
+        'E': sp.E
+    }
+
+    try:
+        expr = sp.sympify(expr_texto, locals=permitidos)
+
+        if expr.free_symbols - {x, y}:
+            raise ValueError(
+                "Solo se permiten las variables x e y."
+            )
+
+        return expr
+
+    except Exception:
+        raise ValueError(
+            "Expresión inválida. Use únicamente funciones matemáticas válidas."
+        )
+def graficar_3d(funcion):
+    x, y = sp.symbols('x y')
+
+    expr = sp.sympify(funcion)
+
+    f = sp.lambdify((x, y), expr, "numpy")
+
+    X = np.linspace(-5, 5, 100)
+    Y = np.linspace(-5, 5, 100)
+
+    X, Y = np.meshgrid(X, Y)
+    Z = f(X, Y)
+
+    fig = plt.figure(figsize=(8,6))
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.plot_surface(X, Y, Z)
+
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+
+    return fig
+def requiere_3d(expr_texto):
+    x, y = sp.symbols('x y')
+
+    try:
+        expr = sp.sympify(expr_texto)
+
+        return y in expr.free_symbols
+
+    except:
+        return False
+def generar_pdf(nombre_pdf,
+                titulo,
+                pasos,
+                resultado,
+                imagen=None):
+
+    doc = SimpleDocTemplate(nombre_pdf)
+
+    estilos = getSampleStyleSheet()
+
+    elementos = []
+
+    elementos.append(
+        Paragraph(titulo, estilos["Title"])
+    )
+
+    elementos.append(Spacer(1, 12))
+
+    elementos.append(
+        Paragraph("<b>Procedimiento:</b>",
+                  estilos["Heading2"])
+    )
+
+    for paso in pasos:
+        elementos.append(
+            Paragraph(paso, estilos["BodyText"])
+        )
+
+    elementos.append(Spacer(1, 12))
+
+    elementos.append(
+        Paragraph(
+            f"<b>Resultado:</b> {resultado}",
+            estilos["BodyText"]
+        )
+    )
+
+    if imagen:
+        elementos.append(Spacer(1, 12))
+        elementos.append(
+            Image(imagen, width=400, height=250)
+        )
+
+    doc.build(elementos)
