1a27fc6533b31b4f4d344f2b81cb2635d950a9f1
arizagaalex97-sys <arizagaalex97@gmail.com>
2026-06-23 00:26:31 -0400

    Actualizar app.py

--- app.py
+++ app.py
@@ -110,6 +110,10 @@ if selected == 'Calculadora':
       x = sp.symbols('x') 
       try:
        f = validar_funcion(func_input)
+       if valor_input.strip().lower() in ['oo', 'inf', 'infinito']:
+           val_num = sp.oo
+       else:
+           val_num = float(valor_input)
       except ValueError as e:
        st.error(f"❌ {e}")
        st.stop()
